bca967d5c0dc5b18b3439e8a6d253b36777eac3d
Mburu540 <edupablo72@gmail.com>
2025-06-26 08:06:41 +0300

    Initial commit 2025-06-26:1

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.include_router(endpoints.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],  # You can restrict this to ["http://localhost:3000"] in production
+    allow_origins=["https://edwardmn.netlify.app"],  # You can restrict this to ["http://localhost:3000"] in production
     allow_credentials=True,
     allow_methods=["*"],  # or ["POST"] if you only use POST
     allow_headers=["*"],
