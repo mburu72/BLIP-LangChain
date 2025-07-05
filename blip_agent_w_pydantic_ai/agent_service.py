95793429df886886ffed10105a72f6e51135da29
Mburu540 <edupablo72@gmail.com>
2025-07-05 16:41:15 +0300

    SOC update 2025-07-05:1

--- blip_agent_w_pydantic_ai/agent_service.py
+++ blip_agent_w_pydantic_ai/agent_service.py
@@ -1,20 +1,16 @@
-import asyncio
 import logging
 from typing import List, TypedDict
-
-from langchain_google_genai import ChatGoogleGenerativeAI
 from pydantic.dataclasses import dataclass
-from pydantic_ai import Agent, RunContext
+from pydantic_ai import Agent
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.providers.google_gla import GoogleGLAProvider
-
+from blip_agent_w_pydantic_ai.tools import a_retrieve
 from config.settings import settings
-from docs_processing.docsops import split_embed, load_docs
 from utils.functions import load_prompt
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 model = GeminiModel(model_name='gemini-2.0-flash-001', provider=GoogleGLAProvider(api_key=settings.G_A_K))
-documents = load_docs('compliance_docs/Kenya/')
 prompt = load_prompt('a_prompt.md')
 
 @dataclass
@@ -24,17 +20,12 @@ class Deps(TypedDict):
     context: list
     answer: str
 
-async def a_retrieve(cxt: RunContext[Deps]):
-    v_store = split_embed(documents)
-    docs = v_store.similarity_search(cxt.prompt)
-    return [d.page_content for d in docs]
-
-
+#Init the agent
 blip_agent = Agent(model = model, deps_type=Deps, retries=2, result_type=str, system_prompt=prompt,
                        tools=[
                         a_retrieve
                        ])
-
+#Run the agent
 async def run_agent(query: str):
     try:
         result = await blip_agent.run(query)
--- blip_agent_w_pydantic_ai/tools.py
+++ blip_agent_w_pydantic_ai/tools.py
@@ -1,7 +1,10 @@
-from typing import List
-
 from pydantic_ai import RunContext
+from docs_processing.docsops import split_embed, load_docs
 
-from docs_processing.docsops import split_embed
+documents = load_docs('compliance_docs/Kenya/')
 
-
+async def a_retrieve(cxt: RunContext[str]):
+    """Get context from available documents using similarity search."""
+    v_store = split_embed(documents)
+    docs = v_store.similarity_search(cxt.prompt)
+    return [d.page_content for d in docs]
