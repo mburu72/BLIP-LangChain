81008fb8429c1b4a070b839696143dc4f0e1efe1
Mburu540 <edupablo72@gmail.com>
2025-07-05 18:01:31 +0300

    update 2025-07-05:2

--- blip_agent_w_pydantic_ai/agent_service.py
+++ blip_agent_w_pydantic_ai/agent_service.py
@@ -1,17 +1,18 @@
 import logging
 from typing import List, TypedDict
 from pydantic.dataclasses import dataclass
-from pydantic_ai import Agent
+from pydantic_ai import Agent, RunContext
 from pydantic_ai.models.gemini import GeminiModel
 from pydantic_ai.providers.google_gla import GoogleGLAProvider
-from blip_agent_w_pydantic_ai.tools import a_retrieve
 from config.settings import settings
+from docs_processing.docsops import split_embed, load_docs
 from utils.functions import load_prompt
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 model = GeminiModel(model_name='gemini-2.0-flash-001', provider=GoogleGLAProvider(api_key=settings.G_A_K))
 prompt = load_prompt('a_prompt.md')
+documents = load_docs('compliance_docs/Kenya/')
 
 @dataclass
 class Deps(TypedDict):
@@ -19,6 +20,12 @@ class Deps(TypedDict):
     question: str
     context: list
     answer: str
+
+async def a_retrieve(cxt: RunContext[Deps]):
+    """Get context from available documents using similarity search."""
+    v_store = split_embed(documents)
+    docs = v_store.similarity_search(cxt.prompt)
+    return [d.page_content for d in docs]
 
 #Init the agent
 blip_agent = Agent(model = model, deps_type=Deps, retries=2, result_type=str, system_prompt=prompt,
--- blip_agent_w_pydantic_ai/tools.py
+++ blip_agent_w_pydantic_ai/tools.py
@@ -1,10 +1,1 @@
-from pydantic_ai import RunContext
-from docs_processing.docsops import split_embed, load_docs
-
-documents = load_docs('compliance_docs/Kenya/')
-
-async def a_retrieve(cxt: RunContext[str]):
-    """Get context from available documents using similarity search."""
-    v_store = split_embed(documents)
-    docs = v_store.similarity_search(cxt.prompt)
-    return [d.page_content for d in docs]
+#As the project progresses, the tools will be added here. For now they will be in the `agent_service.py`
